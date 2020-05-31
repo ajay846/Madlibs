8d807116e0e83e498b28495324afc308db3b4c7d
Ajay <66007868+ajay846@users.noreply.github.com>
2020-05-31 16:39:25 +0530

    Add files via upload

--- Guess_The_Word.py
+++ Guess_The_Word.py
@@ -1,62 +1,65 @@
-guess = ['d','o','g']
+import os
+import sys
+import keyboard
+from time import sleep
 
-user_word_guess_1 = 0
-user_word_guess_2 = 0
-user_word_guess_3 = 0
+main_run = True
 
-count_1 = 0
-count_2 = 0
-count_3 = 0
+while main_run == True:
+    user_word_guess_1 = 0
+    user_word_guess_2 = 0
+    user_word_guess_3 = 0
 
-run = True
+    count_1 = 0
+    count_2 = 0
+    count_3 = 0
 
-print("\nGuess The Word Stored\nYou will have 10 chances to guess each alphabet of the secret word\nThe word here relates to an animal")
+    delay = 1.5
 
+    run = True
 
-while run == True:
-    user_word_guess_1 = input("Enter the guess of the 1st alphabet: \n")
-    count_1 += 1
-    print("You are out of "+str(count_1)+" guesses and left with "+str(10-count_1)+" guesses\n")
-    if count_1 >= 10:
-        run = False
-    if user_word_guess_1 == guess[0]:
-        print("Your 1st alphabet of the secret word is correct!\nNow proceed to the 2nd alphabet\n")
+    print("\nGuess The Word Stored\nYou will have 10 chances to guess each alphabet of the secret word\n")
 
-        while run == True:
-            user_word_guess_2 = input("Enter the guess of the 2nd alphabet: \n")
-            count_2 += 1
-            print("You are out of "+str(count_2)+" guesses and left with "+str(10-count_2)+" guesses\n")
-            if count_2 >= 10:
-                run = False
-            if user_word_guess_2 == guess[1]:
-                print("Your 2nd alphabet of the secret word is correct!\nNow proceed to the 3rd alphabet\n")
+    def split(secret):
+        return[char for char in secret]
 
-                while run == True:
-                    user_word_guess_3 = input("Enter the guess of the 3rd alphabet: \n")
-                    count_3 += 1
-                    print("You are out of "+str(count_3)+" guesses and left with "+str(10-count_3)+" guesses\n")
-                    if count_3 >= 10:
-                        run = False
-                    if user_word_guess_3 == guess[2]:
-                        print("Your guesses were correct! The secret word stored was '"+str(guess)+"'")
+    clear = lambda: os.system("cls")
 
+    secret = input("Any one player should enter the secret word (The number of characters should not exceed 3)\n")
+    clear()
+    if len(secret) != 3:
+        print("The secrect word must be of only 3 characters not less than that, and cannot be a number!")
+        break
 
+    while run == True:
+        user_word_guess_1 = input("Enter the guess of the 1st alphabet: \n")
+        count_1 += 1
+        print("You are out of "+str(count_1)+" guesses and left with "+str(10-count_1)+" guesses\n")
+        if count_1 >= 10:
+            run = False
+        if user_word_guess_1 == secret[0]:
+            print("Your 1st alphabet of the secret word is correct!\nNow proceed to the 2nd alphabet\n")
+            sleep(delay)
+            clear()
 
+            while run == True:
+                user_word_guess_2 = input("Enter the guess of the 2nd alphabet: \n")
+                count_2 += 1
+                print("You are out of "+str(count_2)+" guesses and left with "+str(10-count_2)+" guesses\n")
+                if count_2 >= 10:
+                    run = False
+                if user_word_guess_2 == secret[1]:
+                    print("Your 2nd alphabet of the secret word is correct!\nNow proceed to the 3rd alphabet\n")
+                    sleep(delay)
+                    clear()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    while run == True:
+                        user_word_guess_3 = input("Enter the guess of the 3rd alphabet: \n")
+                        count_3 += 1
+                        print("You are out of "+str(count_3)+" guesses and left with "+str(10-count_3)+" guesses\n")
+                        if count_3 >= 10:
+                            run = False
+                        if user_word_guess_3 == secret[2]:
+                            print("Your guesses were correct! The secret word stored was '"+str(guess)+"'")
+                            sleep(delay)
+                            clear()
